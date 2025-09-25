eb4e32c45d968d98c088a0791bc79829e2970347
CubaEtecsa <dailysd88@gmail.com>
2025-09-25 15:40:31 -0400

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-// Configuración de la App Facebook
-const CONFIG = {
-    APP_ID: '25099674832973397', // Tu App ID
-    APP_VERSION: 'v19.0',
-    SCOPE: 'pages_manage_posts,pages_read_engagement,pages_show_list'
-};
+
